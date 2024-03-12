8c59f89c020c73bb501c260741091fb1397207c4
Amit Thakur <107143310+Amitonhub@users.noreply.github.com>
2024-03-12 12:54:48 +0530

    added src

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ParallaxCompoent from "./Parallax";
+
+function App() {
+  return (
+    <div className="App">
+      <ParallaxCompoent />
+    </div>
+  );
+}
+
+export default App;
